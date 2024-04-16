8b630c02ff9cd1b71d72bc6053747676c884c76a
wjsxorjs <wjsxorjs@naver.com>
2024-04-16 14:43:24 +0900

    SQl 2024.04.16 0.6

--- Ex20240416/W2D3_PM.sql
+++ Ex20240416/W2D3_PM.sql
@@ -1,5 +1,24 @@
--- 
--- 
+-- ====================| 2024.04.16 PM |=========================
+-- 자원 삭제 : DELETE
+
+-- m_idx가 4번인 자원을 삭제하라
+DELETE
+FROM member_t
+WHERE m_idx = 4
+;
+
+DELETE
+FROM member_t
+WHERE m_idx > 2
+;
+
+
+
+-- 데이터 확인
+SELECT *
+FROM member_t
+;
+
 -- 
 -- 
 -- 
---
+++ Ex20240416/W2D3_PM_Extra.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM member_t
+;
